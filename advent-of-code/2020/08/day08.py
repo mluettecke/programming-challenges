563fb8fb70d09950b45fcbe5d85f14169a47c98c
Marcel L <mluettecke@posteo.net>
2020-12-08 23:28:44 +0100

    Refactored and added solution for day 8 of advent of code 2020

--- advent-of-code/2020/08/day08.py
+++ advent-of-code/2020/08/day08.py
@@ -5,14 +5,12 @@
 import os
 
 
-def part_one(instructions):
-    """
-    """
+def run_instructions(instructions):
     i = 0
     accumulator = 0
     run_instructions = set()
     while i < len(instructions) and i not in run_instructions:
-        operation, argument = instructions[i].split(" ")
+        operation, argument = instructions[i]
         argument = int(argument)
         run_instructions.add(i)
         if operation == "acc":
@@ -22,18 +20,42 @@ def part_one(instructions):
             i += argument
         elif operation == "nop":
             i += 1
-    return accumulator
+    finished = i == len(instructions)
+    return [finished, accumulator]
 
 
-def part_two(input_data):
+def part_one(instructions):
     """
     """
-    pass
+    return run_instructions(instructions)
+
+
+def part_two(instructions):
+    """
+    """
+    for i in range(len(instructions)):
+        operation, argument = instructions[i]
+        # switch instructions around til it fits
+        if operation == "jmp":
+            instructions[i][0] = "nop"
+            finished, accumulator = run_instructions(instructions)
+            instructions[i][0] = "jmp"
+        elif operation == "nop":
+            instructions[i][0] = "jmp"
+            finished, accumulator = run_instructions(instructions)
+            instructions[i][0] = "nop"
+
+        if finished:
+            return accumulator
 
 
 def read_input():
+    instructions = []
     with open(os.path.join(os.path.dirname(__file__), "input"), "r") as f:
-        return [line for line in f.read().splitlines()]
+        for line in f.read().splitlines():
+            operation, argument = line.split(" ")
+            instructions.append([operation, int(argument)])
+    return instructions
 
 
 def main():
